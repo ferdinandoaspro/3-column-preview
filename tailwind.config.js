a5821b4cfc5ca8bdaed8a25e44feda6059bf20a5
FatChickn <ferdinandoaspro2@gmail.com>
2023-01-27 02:06:44 +0100

    work done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,19 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    colors: {
+    extend: {
+      colors: {
       'bright-orange': "hsl(31, 77%, 52%)",
       "dark-cyan": "hsl(184, 100%, 22%)",
       "very-dark-cyan": "hsl(179, 100%, 13%)",
-      "transparent-white": "hsla(0, 0%, 100%, 0.75)",
-      "very-light-gray": "hsl(0, 0%, 95%)"
-    },
+      "transparent-white": "hsla(0, 0%, 100%, 0.75)", // paragraphs //
+      "very-light-gray": "hsl(0, 0%, 95%)" // background, headings, buttons //
+      },
+      fontFamily: {
+        "display": "'Big Shoulders Display', cursive", // font weight: 700 //
+        "lexend": "'Lexend Deca', sans-serif" //font-weight: 400 //
+      }
+    }
   },
   plugins: [],
 }
